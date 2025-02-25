dd03b609830c4320c21a9ac5a51774725fbb6d4d
gabrieliwa <spades_smugger.0e@icloud.com>
2025-02-25 22:41:37 +0100

    minor changes

---
+++ common-functions.js
@@ -0,0 +1,30 @@
+// common-functions.js
+
+function generateUID(nome, cognome, email, telefono) {
+    // ... (your existing code) ...
+}
+
+function isInputValid() {
+    // ... (your existing code) ...
+}
+function existEmail(email, index) {
+    // ... (your existing code) ...
+}
+
+function existTelefono(telefono, index) {
+ // ... (your existing code) ...
+}
+function addFriend() {
+ // ... (your existing code) ...
+}
+
+function removeFriend() {
+ // ... (your existing code) ...
+}
+function saveFormData() {
+ // ... (your existing code) ...
+}
+
+function dateNow() {
+ // ... (your existing code) ...
+}
